26d8b02c6956b028b1892f6e4d50dcea43f161ab
Leo0811015 <90439630+Leo0811015@users.noreply.github.com>
2022-01-07 13:49:33 +0800

    print my name with line break

---
+++ fetch_input.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = a;
+    int b = b;
+    int c = c;
+    int d = d;
+    int e = e;
+
+    printf("請輸入五個數字，以空白隔開。\n");
+    //add scanf code to get data from input
+    //and feed those input into a, b, c, d, e, respectively.
+    //only ONE line code is you need
+    //please refer page 18 of slides
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+    scanf("%d", &e);
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+}
